45d91739a2dfa9ad362ad0a028e91f2aca140ae3
Matteo Rota <m.rota50@studenti.unibg.it>
2022-09-09 17:12:14 +0100

    first steps fine tuning balancing control

--- balancer.py
+++ balancer.py
@@ -5,32 +5,45 @@ from ballTracking import preprocess, isolatePlate, isolateBall
 from gpiozero.pins.pigpio import PiGPIOFactory
 from gpiozero import AngularServo
 from simple_pid import PID
+import time
+import matplotlib.pyplot as plt
+
 
 # needs daemon pigpio to be running: on this machine it starts on startup
-servoX = AngularServo(17, pin_factory = PiGPIOFactory(), min_angle = 90, max_angle = -90, max_pulse_width = 0.00255, min_pulse_width = 0.0004)
-servoY = AngularServo(27, pin_factory = PiGPIOFactory(), min_angle = 90, max_angle = -90, max_pulse_width = 0.00255, min_pulse_width = 0.0004)
+servoX = AngularServo(17, pin_factory = PiGPIOFactory(), min_angle = -90, max_angle = 90, max_pulse_width = 0.00255, min_pulse_width = 0.0004)
+servoY = AngularServo(27, pin_factory = PiGPIOFactory(), min_angle = -90, max_angle = 90, max_pulse_width = 0.00255, min_pulse_width = 0.0004)
 
-
-PROP = 1.1
-Kp = 0.03 * .7 * PROP
-Ki = 0.012 * 2.2 * PROP
-Kd = 0.01875 * 3.5 * PROP
+PROP = 4.5 # 1.1 (in realt 5.5 con altri valori default ha performato bene)
+Kp = 0.675 # 0.03 * .7 * PROP
+Ki = 0 # 0.012 * 2.2 * PROP
+Kd = 0 # 0.01875 * 3.5 * PROP
+#valori da provare con la mediana
+# PROP = 4.5
+# Kp = 1
+# Ki = 0.5
+# Kd = 0.5
 
 controllerX = PID(Kp, Ki, Kd, sample_time = 0.0001)
-controllerX.output_limits = (-20, 20)
+controllerX.output_limits = (-90, 90)
 controllerY = PID(Kp, Ki, Kd, sample_time = 0.0001)
-controllerY.output_limits = (-20, 20)
+controllerY.output_limits = (-90, 90)
+
+ballXs = []
+ballYs = []
+times = []
 
 
 video = cv2.VideoCapture(0)
 
-while True:
+i = 0
+input("Press a key to start getting fucked by this balance your ugly ass balls type machine")
+while i < 500: # while true:
     _, img = video.read()
     
     while img is None:
         pass    
     
-    # ball and center of plate detection 
+    # detection of ball and center of plate 
     img = preprocess(img)
     targetX, targetY, img = isolatePlate(img)
     ballX, ballY, img, imgDebug = isolateBall(img)
@@ -40,18 +53,32 @@ while True:
     imgDebug = cv2.line(imgDebug, (int(targetX) - sz, int(targetY)), (int(targetX) + sz, int(targetY)), (0, 255, 0), thickness = 2)
     imgDebug = cv2.line(imgDebug, (int(targetX), int(targetY) - sz), (int(targetX), int(targetY) + sz), (0, 255, 0), thickness = 2)   
     
+    
+    # balancing control
     if ballX is not None and ballY is not None:
+        # for plotting
+        ballXs.append(ballX)
+        ballYs.append(ballY)
+        times.append(int(time.time()))
+        
+        
         controllerX.setpoint = targetX
         controllerY.setpoint = targetY
-
         controlX = controllerX(ballX)
-        controlY = controllerY(ballY)
+        controlY = controllerY(ballY)   
 
         servoX.angle = controlX
         servoY.angle = controlY
+        
+        
+        plt.plot(times, ballXs, '-')
+        
     
     cv2.imshow("Debug", imgDebug)
     cv2.waitKey(1)
+    i = i + 1
 
 video.release()
-cv2.destroyAllWindows() 
+cv2.destroyAllWindows()
+
+plt.show()
