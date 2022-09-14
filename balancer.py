7d6dd36d27e6a3e43c531ec8813ac52bb87aa7ac
Matteo Rota <m.rota50@studenti.unibg.it>
2022-09-14 15:45:38 +0100

    fine tuned balancing control all float

--- balancer.py
+++ balancer.py
@@ -12,18 +12,19 @@ servoX = AngularServo(17, pin_factory = PiGPIOFactory(), min_angle = -90, max_an
 servoY = AngularServo(27, pin_factory = PiGPIOFactory(), min_angle = -90, max_angle = 90, max_pulse_width = 0.00245, min_pulse_width = 0.0004)
 
 
-# Ziegler-Nichols + tests
-PROP = 5.5 # 1.1 5.5 con altri valori default ha performato bene
-Kp = 0.03 * .7 * PROP
-Ki = 0.012 * 2.2 * PROP
-Kd = 0.01875 * 3.5 * PROP
+# determined via tests
+Kp = 0.53 
+Ki = 0.15 
+Kd = 0.27
 
 # setpoint yet to determine
 controllerX = PID(Kp, Ki, Kd, sample_time = 0.0001)
-controllerX.output_limits = (-90, 90)
+controllerX.output_limits = (-35, 35)
 controllerY = PID(Kp, Ki, Kd, sample_time = 0.0001)
-controllerY.output_limits = (-90, 90)
+controllerY.output_limits = (-35, 35)
 
+previousBallX = []
+previousBallY = []
 previousControlX = []
 previousControlY = []
 
@@ -44,9 +45,7 @@ while True:
     if setup:
         targetX, targetY, img = isolatePlate(img) # recalculating center every time generates very variable setpoint which makes control noisy
         controllerX.setpoint = targetX
-        controllerY.setpoint = targetY       
-#         controllerX.setpoint = int(targetX)
-#         controllerY.setpoint = int(targetY)
+        controllerY.setpoint = targetY
         setup = False
     else:
         _, _, img = isolatePlate(img)
@@ -60,28 +59,22 @@ while True:
     
     # balancing control
     if ballX is not None and ballY is not None:
-#         ballX = kalmanX.getEstimate(ballX)[0]
-#         ballY = kalmanY.getEstimate(ballY)[0]
+        previousBallX.append(ballX)
+        previousBallY.append(ballY)
         
         controlX = controllerX(ballX)
         controlY = controllerY(ballY)
-#         controlX = controllerX(int(ballX))
-#         controlY = controllerY(int(ballY))
-        
+
         previousControlX.append(controlX)
         previousControlY.append(controlY)
-
-        if len(previousControlX) >=5:
-            controlX = np.mean(previousControlX[-4 : len(previousControlX)])
-            controlY = np.mean(previousControlY[-4 : len(previousControlY)])
-
+        
         servoX.angle = controlX
         servoY.angle = controlY
         
         # add control input to debug image
-        imgDebug = cv2.putText(imgDebug, "setpointX - ballX: " + str(int(controllerX.setpoint) - int(ballX)) + " | setpointY - ballY: " + str(int(controllerY.setpoint) - int(ballY)), (int(imgDebug.shape[1] / 3), int(imgDebug.shape[0] - 30)), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (0, 0, 255))
-        imgDebug = cv2.putText(imgDebug, "X: " + str(int(controlX)) + ", Y: " + str(int(controlY)), (int(imgDebug.shape[1] / 2), int(imgDebug.shape[0] - 15)), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (0, 0, 255))
- 
+        imgDebug = cv2.putText(imgDebug, "setpointX - ballX: " + str(round(controllerX.setpoint - ballX, 2)) + " | setpointY - ballY: " + str(round(controllerY.setpoint - ballY, 2)), (int(imgDebug.shape[1] / 3), int(imgDebug.shape[0] - 30)), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (0, 0, 255))
+        imgDebug = cv2.putText(imgDebug, "X: " + str(round(controlX, 2)) + ", Y: " + str(round(controlY, 2)), (int(imgDebug.shape[1] / 2), int(imgDebug.shape[0] - 15)), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (0, 0, 255))
+
     
     cv2.imshow("Debug", imgDebug)
     cv2.waitKey(1)
